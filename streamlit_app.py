1611022be3d84050fe0fc0ff7a4a37a440939ebe
Sandeep-cmd1 <wabisabi231218@gmail.com>
2025-11-15 19:56:35 +0530

    Refactor model loading and enable legacy saving
    Updated model loading method and enabled legacy saving for Keras.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import tensorflow as tf
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing import sequence
 from tensorflow.keras.models import load_model
+import keras
+keras.config.enable_legacy_saving()
 
 
 #Load IMDB dataset word indedx to be used to encode/decode user review input
@@ -12,8 +14,7 @@ reverse_word_index = {value:key for key, value in word_index.items()}
 
 #Load pre-trained model with ReLU activation fn
 #model = load_model('simple_RNN_imdb_r0.h5')
-from tensorflow.keras.saving.legacy import hdf5_format
-model = hdf5_format.load_model_from_hdf5('simple_RNN_imdb_r0.h5')
+model = load_model('simple_RNN_imdb_r0.h5', compile=False)
 
 
 #Helper functions for encoding & padding user reviews and decoding embedded reviews 
@@ -54,5 +55,6 @@ if st.button('Classify'):
 else:
 
   st.write('Please enter a movie review')
+
 
 
